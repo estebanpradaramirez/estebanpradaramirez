fbc4e135954ce5948aa523046d93690a3c82cfa9
estebanpradaramirez <estebanprivado0@gmail.com>
2025-05-26 21:07:09 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function searchTires() {
+  const vehicleType = document.getElementById("vehicleType").value.toLowerCase();
+  const brand = document.getElementById("brand").value.toLowerCase();
+  const reference = document.getElementById("reference").value.toLowerCase();
+
+  const cards = document.querySelectorAll(".tire-card");
+  cards.forEach
+::contentReference[oaicite:0]{index=0}
+ 
